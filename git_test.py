60cdb90e47852d2470b716a34a2d31db35f6c040
JiangZongKang <jzksuccess@gmail.com>
2018-10-01 19:25:53 +0800

    write a test python file

---
+++ git_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+"""
+@author: JiangZongKang
+@contact: top_jzk@163.com
+@file: git_test.py.py
+@time: 2018/10/1 19:23
+
+"""
+
+print('hello python!')
+
+print('hello git!')
